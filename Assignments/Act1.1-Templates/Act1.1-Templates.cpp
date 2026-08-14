167a4d0d9ddf86f6372d59bd8b9e7657b2d2ff70
Joaquin López <joaquinloho@gmail.com>
2026-08-14 16:11:53 -0600

    Act1.1-template suma 15

--- Assignments/Act1.1-Templates/Act1.1-Templates.cpp
+++ Assignments/Act1.1-Templates/Act1.1-Templates.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int sum(int a, int b){
+    return a + b;
+};
+
+int main(){
+    int a = 5;
+    int b = 10;
+    cout << "La suma entre " << a << " y " << b << " es: " << sum(a,b) << endl;
+    return 0;
+
+}
