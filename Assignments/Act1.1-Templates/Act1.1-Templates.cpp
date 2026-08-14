aeb100376b8ee34263bbdee028b3080512f5fb04
Joaquin López <joaquinloho@gmail.com>
2026-08-14 16:27:43 -0600

    Act1.1 Con template functions

--- Assignments/Act1.1-Templates/Act1.1-Templates.cpp
+++ Assignments/Act1.1-Templates/Act1.1-Templates.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 using namespace std;
-
+/*
 int sum(int a, int b){
     return a + b;
 };
 
+int sum(double a, double b){
+    return a + b;
+};
+
+string sum(string a , string b){
+    return a + b;
+    
+};
+*/
+
+//Template hace todos los tipos de datos.
+template <typename T>
+T sum(T a, T b){
+    return a + b;
+}
+
+
+
 int main(){
-    int a = 5;
-    int b = 10;
+    string a = "hola ";
+    string b = "crayola";
     cout << "La suma entre " << a << " y " << b << " es: " << sum(a,b) << endl;
+
+    int c = 5;
+    int d = 10;
+    cout << "La suma entre " << c << " y " << d << " es: " << sum(c,d) << endl;
+
+    double e = 5.5;
+    double f = 10.5;
+    cout << "La suma entre " << e << " y " << f << " es: " << sum(e,f) << endl;
+
+
     return 0;
 
 }
